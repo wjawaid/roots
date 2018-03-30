d9882f43f3ae8d62160ed4aba6d65069c2ce979e
Wajid Jawaid <wj241@cam.ac.uk>
2018-03-30 12:45:11 +0100

    Included dpt() function

---
+++ R/dpt.r
@@ -0,0 +1,15 @@
+##' Calculate pseudotime
+##'
+##' Calculate diffusion pseudotime
+##' @title Calculate pseudotime
+##' @param dmap Output from diffuseMat function
+##' @param startIndex Numerical index of starting cell
+##' @return Vector of pseudotimes
+##' @author Wajid Jawaid
+##' @export
+dpt <- function(dmap, startIndex) {
+    vd <- (dmap$vectors - dmap$vectors[,startIndex])^2
+    w <- dmap$values / (1 - dmap$values)
+    pt <- t(t(vd) * w)
+    colSums(pt)
+}
